4b0b46f917e8ec42b2eebce84f4c49481ed792f3
guanhongfu <guanhongfu@tenwit.com>
2022-03-29 21:19:02 +0800

    feat: 增加 node:16

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 
 基于 node:16 & node:16-alpine3.14
 
+## 打镜像并上传 Dockerhub
 ```shell
 # 执行前 docker login 成功
 ./build-node-16.sh
 ./build-node-16-alpine.sh
 ```
+
+## 使用
+```shell
+docker pull chf007/pnpm
+docker pull chf007/pnpm:6.32.3-alpine
+```
+
---
+++ build-node-16-alpine.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# build，可选参数 `--no-cache`
+echo '打包镜像...'
+export CONTAINER_REGISTRY=docker.io
+export CONTAINER_REGISTRY_NAMESPACE=chf007
+export IMAGE_NAME=pnpm
+export IMAGE_VERSION=6.32.3-alpine
+export DOCKER_BUILDKIT=1
+docker build --pull \
+--build-arg IMAGE_VERSION=$IMAGE_VERSION \
+--build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
+-t $CONTAINER_REGISTRY/$CONTAINER_REGISTRY_NAMESPACE/$IMAGE_NAME:$IMAGE_VERSION \
+-f Dockerfile-node-16-alpine . || exit 1
+
+# push
+echo '推送镜像...'
+docker push $CONTAINER_REGISTRY/$CONTAINER_REGISTRY_NAMESPACE/$IMAGE_NAME:$IMAGE_VERSION
+
+
